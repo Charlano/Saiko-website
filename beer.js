e440ddc6d4e6dfb98e97ce747646d6921a42e909
Charlano <andreazhou08@gmail.com>
2025-01-12 19:52:44 +0100

    .

--- beer.js
+++ beer.js
@@ -6,10 +6,9 @@ const slideTexts = [
     {
         description: 'La nostra birra più leggera e versatile, una PILS (lager) che esprime tutta la purezza della tradizione birraria giapponese. Fresca e dissetante, è l’abbinamento perfetto per una cena a base di riso, zuppe ma anche involtini primavera, uramaki e tanti altri.',
         nutritionalInfo: [
-            { label: 'Calorie', value: '150 kcal' },
-            { label: 'Alcohol', value: '5%' },
-            { label: 'Zucchero', value: '3g' },
-            { label: 'Carboidrati', value: '10g' },
+            { label: 'Volume', value: '0,33L' },
+            { label: 'Alcohol', value: '4,8%vol.' },
+            { label: 'Ingredienti', value: 'acqua, malto d\'orzo, frumento, avena, lievito' },
         ],
         pretitle: 'Fresca e versatile',
         title: 'Informazioni sulla birra Yukio',
@@ -18,10 +17,9 @@ const slideTexts = [
     {
         description: 'Birra IPA che, grazie al suo perfetto equilibrio tra l\'amarezza del luppolo e la dolcezza del malto caramellato, si abbina perfettamente a piatti piccanti o a qualsiasi piatto fritto (come la tempura) che desideri offrire ai tuoi clienti.',
         nutritionalInfo: [
-            { label: 'Calorie', value: '150 kcal' },
-            { label: 'Alcohol', value: '5%' },
-            { label: 'Zucchero', value: '3g' },
-            { label: 'Carboidrati', value: '10g' },
+            { label: 'Volume', value: '0,33L' },
+            { label: 'Alcohol', value: '6%vol.' },
+            { label: 'Ingredienti', value: 'acqua, malto d\'orzo, luppolo, lievito' },
         ],
         pretitle: 'Elegante ed agrumato',
         title: 'Informazioni sulla birra Itoku',
@@ -30,10 +28,9 @@ const slideTexts = [
     {
         description: 'Birra BLANCHE molto pregiata, caratterizzata da una luppolatura con la varietà CASCADE e arricchita dall’aggiunta di scorza d’arancia amara. Perfetta per accompagnare il sashimi, si sposa magnificamente con tutti i piatti a base di pesce.',
         nutritionalInfo: [
-            { label: 'Calorie', value: '150 kcal' },
-            { label: 'Alcohol', value: '5%' },
-            { label: 'Zucchero', value: '3g' },
-            { label: 'Carboidrati', value: '10g' },
+            { label: 'Volume', value: '0,33L' },
+            { label: 'Alcohol', value: '4,3%vol.' },
+            { label: 'Ingredienti', value: 'acqua, malto d\'orzo, fiocchi di frumento, luppolo, lievito, buccia d\'arancia'},
         ],
         pretitle: 'Intenso ma bilanciato',
         title: 'Informazioni sulla birra Sujin',
